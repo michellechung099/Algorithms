d1172b2df74e6dc7715ddc00922b9c7c7f2307a4
michelle.chung <mchung099@gmail.com>
2023-09-03 23:24:10 -0700

    O(n^2) twoSum

--- algo.py
+++ algo.py
@@ -12,3 +12,13 @@ def binary_search(list, item):
         else:
             low = mid + 1
     return None
+
+# brute force, O(n^2) time complexity
+def twoSum(nums, target):
+    n = len(nums)
+
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
