2ff14cee0b3f53a12bdd73fed9235b868c193242
michelle.chung <mchung099@gmail.com>
2023-09-05 20:16:53 -0700

    best time to buy and sell stock O(n)

--- algo.py
+++ algo.py
@@ -53,3 +53,15 @@ def twoSum(nums, target):
 
 # Array 2
 # best time to buy and sell stock
+def maxProfit(prices):
+    left, right = 0, 1
+    maxP = 0
+
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            maxP = max(maxP, profit)
+        else:
+            left = right
+        right += 1
+    return maxP
