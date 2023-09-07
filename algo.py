3f3a8ed8b4356d9cfb4539568f69374e9f1c0c61
michelle.chung <mchung099@gmail.com>
2023-09-07 13:29:50 -0700

    isPalindrome

--- algo.py
+++ algo.py
@@ -78,3 +78,65 @@ def containsDuplicate(nums: List[int]) -> bool:
             return True
         seen[num] = seen.get(num, 0) + 1
     return False
+
+# Array 4
+# product of array except self O(n)
+# def productExceptSelf(nums: List[int]) -> List[int]:
+
+
+# Array 5
+# maximum subarray
+
+# Array 6
+# maximum product subarray
+
+
+# Array 7
+# find min in rotated sorted array
+
+
+# Array 8
+# search in rotated sorted array
+
+# Array 9
+# 3 sum
+
+
+# Array 10
+# container with most water
+
+
+# Two Pointers 1
+# Valid Palindrome
+def isPalindrome(s):
+    cleaned = ''.join(char for char in s.lower() if char.isalnum())
+    return cleaned == cleaned[::-1]
+
+# another solution in O(n) time complexity
+def isPalindrome(s):
+    newStr = ""
+
+    for c in s:
+        if c.isalnum():
+            newStr += c.lower()
+    return newStr == newStr[::-1]
+
+# another solution with O(1) memory and O(n) with two pointers
+def isPalindrome(s: str) -> bool:
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        while l < r and not alphaNum(s[l]):
+            l += 1
+        while r > l and not alphaNum(s[r]):
+            r -= 1
+        if s[l].lower() != r[l].lower()
+            return False
+        l, r = l + 1, r - 1
+    return True
+
+# helper function to determine if the element is alphanum based on ASCII contiguous position
+def alphaNum(c):
+    return (ord('A') <= c <= ord('Z') or
+            ord('a') <= c <= ord('z') or
+            ord('0') <= c <= ord('9'))
